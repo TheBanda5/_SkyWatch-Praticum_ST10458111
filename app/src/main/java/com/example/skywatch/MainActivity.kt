6cf39760c0766e657b446ba8a970f4866794810d
Bered Kabuluku_ST10458111 <ST10458111@vcconnect.edu.za>
2024-06-10 09:44:33 +0200

    add new things

--- app/src/main/java/com/example/skywatch/MainActivity.kt
+++ app/src/main/java/com/example/skywatch/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.skywatch
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -13,7 +14,13 @@ class MainActivity : AppCompatActivity() {
         val btnExit = findViewById<Button>(R.id.btnExit)
 
         btnNext.setOnClickListener{
+            // Move to next page
+            val intent = Intent(this, MainScreen::class.java)
+        }
 
+        btnExit.setOnClickListener{
+            // CLose the app
+            finish()
         }
     }
 }
