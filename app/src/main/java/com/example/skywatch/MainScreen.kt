763d866d7ae8ce4526ad2fee355f3e9d4c9ef98c
Bered Kabuluku_ST10458111 <ST10458111@vcconnect.edu.za>
2024-06-10 10:44:58 +0200

    add new things

--- app/src/main/java/com/example/skywatch/MainScreen.kt
+++ app/src/main/java/com/example/skywatch/MainScreen.kt
@@ -1,11 +1,54 @@
 package com.example.skywatch
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 
 class MainScreen : AppCompatActivity() {
+
+    private var temperatures = arrayOf(20, 22, 25, 23, 24, 21, 19) // Example of temperatures for each day of the week
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_main_screen)
+
+        val detailsButton = findViewById<Button>(R.id.detailsButton)
+        val editText = findViewById<EditText>(R.id.editText)
+        val clearButton = findViewById<Button>(R.id.clearButton)
+        val exitButton = findViewById<Button>(R.id.exitButton)
+
+        // Calculate the average temperature of the week
+        val averageTemperature = calculateAverageTemperature()
+
+        // Display the temperature
+        "Average temperature of the week: $averageTemperature °C".also { editText.text = it }
+
+
+        detailsButton.setOnClickListener {
+            val intent = Intent(this, DetailledView::class.java)
+            startActivity(intent)
+        }
+
+        clearButton.setOnClickListener {
+            clearData()
+        }
+
+        exitButton.setOnClickListener {
+            finish()
+        }
+    }
+
+    private fun calculateAverageTemperature(): Double {
+        var sum = 0
+        for (temperature in temperatures) {
+            sum += temperature
+        }
+        return sum.toDouble() / temperatures.size
+    }
+
+    private fun clearData() {
+        // reset data if necessary
     }
 }
