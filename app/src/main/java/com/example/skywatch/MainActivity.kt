6cc552451def436486db40682770d330666f4eb8
Bered Kabuluku_ST10458111 <ST10458111@vcconnect.edu.za>
2024-06-10 09:26:53 +0200

    add new things

--- app/src/main/java/com/example/skywatch/MainActivity.kt
+++ app/src/main/java/com/example/skywatch/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.skywatch
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnNext = findViewById<Button>(R.id.btnNext)
+        val btnExit = findViewById<Button>(R.id.btnExit)
+
+        btnNext.setOnClickListener{
+            intent = Intent(this, )
+        }
     }
 }
